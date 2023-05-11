4b2ad0a029bf067bf0aef1c1f21beffbbb8dddf9
Pablo Di Marco <pablodimarco7@gmail.com>
2023-05-11 13:00:02 -0300

    Mostrando alertas en los campos

--- js/app.js
+++ js/app.js
@@ -5,6 +5,7 @@ document.addEventListener('DOMContentLoaded', function() {
     const inputEmail = document.querySelector('#email');
     const inputAsunto = document.querySelector('#asunto');
     const inputMensaje = document.querySelector('#mensaje');
+    const formulario = document.querySelector('#formulario');
 
     // asignar eventos
     // blur se ejecuta cuando abandonas un campo
@@ -20,7 +21,30 @@ document.addEventListener('DOMContentLoaded', function() {
     inputMensaje.addEventListener('blur', validar);
 
     function validar(e) {
-        console.log(e.target.value);
+        // de esta manera accedemos al dato del input
+        // console.log(e.target.value);
+        // parentElement selecciona el elemento padre
+        // nextElementSibling indica al hermano del padre
+
+        // con esta condicion verificamos si el campo esta vacio o no
+        if(e.target.value.trim() === ''){
+            // de esta forma haces mas dinamico el text
+            mostrarAlerta(`El campo ${e.target.id} es obligatorio`, e.target.parentElement);
+        } else ;
     }
 
-})
+    function mostrarAlerta(mensaje, referencia) {
+        // generar alerta en HTML
+        const error = document.createElement('P');
+        error.textContent = mensaje;
+        // agregar clases a la alerta
+        error.classList.add('bg-red-600', 'text-white', 'p-2', 'text-center')
+
+        // inyectar el error al formulario
+        // append child agrega un hijo al padre, lo agrega al final siempre
+        // agregando como parametro la referencia lo agrega al final de cada input al mensaje
+        referencia.appendChild(error);
+
+    }
+
+});
