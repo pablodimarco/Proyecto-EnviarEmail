cc2115b40a28154c2d268e10859a7a131d26000b
Pablo Di Marco <pablodimarco7@gmail.com>
2023-05-14 20:04:12 -0300

    Agregando contenido extra al formulario

--- js/app.js
+++ js/app.js
@@ -1,95 +1,107 @@
-// domcontentloaded carga el dom
-// simulador de envios de mail
 document.addEventListener('DOMContentLoaded', function() {
 
-    // creando un objeto para verificar que los campos esten llenos
     const email = {
         email: '',
         asunto: '',
         mensaje: ''
     }
 
-    // seleccionar elementos de la interfaz
+    // Seleccionar los elementos de la interfaz
     const inputEmail = document.querySelector('#email');
     const inputAsunto = document.querySelector('#asunto');
     const inputMensaje = document.querySelector('#mensaje');
     const formulario = document.querySelector('#formulario');
-    const btnSubmit = document.querySelector('#formulario button[type="submit"]')
+    const btnSubmit = document.querySelector('#formulario button[type="submit"]');
+    const btnReset = document.querySelector('#formulario button[type="reset"]');
+    const spinner = document.querySelector('#spinner');
+    const cc = document.querySelector('#cc');
 
-    // asignar eventos
-    // blur se ejecuta cuando abandonas un campo
-    // input se ejecuta cada vez que se escribe dentro del campo
-    inputEmail.addEventListener('input', function(e) {
-
-        // de esta forma podemos acceder al valor dentro del input
-      console.log(e.target.value);
-    });
-
-    // de esta forma podemos escribir codigo reutilizable 
-    // input lo que hace es que todo se ejecute en tiempo real
+    // Asignar eventos
+    inputEmail.addEventListener('input', validar);
     inputAsunto.addEventListener('input', validar);
     inputMensaje.addEventListener('input', validar);
 
-    function validar(e) {
-        // de esta manera accedemos al dato del input
-        // console.log(e.target.value);
-        // parentElement selecciona el elemento padre
-        // nextElementSibling indica al hermano del padre
-
-        // con esta condicion verificamos si el campo esta vacio o no
-        if(e.target.value.trim() === ''){
-            // de esta forma haces mas dinamico el text
-            mostrarAlerta(`El campo ${e.target.id} es obligatorio`, e.target.parentElement);
-            email[e.target.name] = '';
-            comprobarEmail();
-            return; // este return detiene la ejecucion del codigo
-        } 
-
-        // de esta forma mostrar alerta solo se ejecuta si el id es igual a email y la funcion validarEmail falla
-        if (e.target.id === 'email' && !validarEmail(e.target.value)){
-            mostrarAlerta('el mail no es valido', e.target.parentElement);
-            email[e.target.name] = '';
+    // creando contenido extra
+    cc.addEventListener('input', function(e) {
+        e.preventDefault();
+        if(e.target.value.trim() !== ''){
             comprobarEmail();
         }
+    });
 
-        // esta funcion limpia la alerta cuando se completan los campos
+    formulario.addEventListener('submit', enviarEmail);
+
+    btnReset.addEventListener('click', function(e) {
+        e.preventDefault();
+        resetFormulario();
+    });
+
+
+    function enviarEmail(e) {
+        e.preventDefault();
+
+        spinner.classList.add('flex');
+        spinner.classList.remove('hidden');
+
+        setTimeout(() => {
+            spinner.classList.remove('flex');
+            spinner.classList.add('hidden');
+
+            resetFormulario();
+
+            // Crear una alerta
+            const alertaExito = document.createElement('P');
+            alertaExito.classList.add('bg-green-500', 'text-white', 'p-2', 'text-center', 'rounded-lg', 'mt-10', 'font-bold', 'text-sm', 'uppercase');
+            alertaExito.textContent = 'Mensaje enviado correctamente';
+
+            formulario.appendChild(alertaExito);
+
+            setTimeout(() => {
+                alertaExito.remove(); 
+            }, 3000);
+        }, 3000);
+    }
+
+    function validar(e) {
+        if(e.target.value.trim() === '') {
+            mostrarAlerta(`El Campo ${e.target.id} es obligatorio`, e.target.parentElement);
+            email[e.target.name] = '';
+            comprobarEmail();
+            return;
+        }
+
+        if(e.target.id === 'email' && !validarEmail(e.target.value)) {
+            mostrarAlerta('El email no es válido', e.target.parentElement);
+            email[e.target.name] = '';
+            comprobarEmail();
+            return;
+        }
+
         limpiarAlerta(e.target.parentElement);
 
-        // asignar valores al objeto
+        // Asignar los valores
         email[e.target.name] = e.target.value.trim().toLowerCase();
 
-        // comprobar email mediante funcion
+        // Comprobar el objeto de email
         comprobarEmail();
     }
 
     function mostrarAlerta(mensaje, referencia) {
-
         limpiarAlerta(referencia);
-
-        // comprueba si ya existe una alerta
-        const alerta = referencia.querySelector('.bg-red-600');
-        if(alerta) {
-            alerta.remove();
-        }
-
-
-
-        // generar alerta en HTML
+        
+        // Generar alerta en HTML
         const error = document.createElement('P');
         error.textContent = mensaje;
-        // agregar clases a la alerta
-        error.classList.add('bg-red-600', 'text-white', 'p-2', 'text-center')
-
-        // inyectar el error al formulario
-        // append child agrega un hijo al padre, lo agrega al final siempre
-        // agregando como parametro la referencia lo agrega al final de cada input al mensaje
+        error.classList.add('bg-red-600', 'text-white', 'p-2', 'text-center');
+       
+        // Inyectar el error al formulario
         referencia.appendChild(error);
-
     }
 
-    function limpiarAlerta(referencia){
+    function limpiarAlerta(referencia) {
+        // Comprueba si ya existe una alerta
         const alerta = referencia.querySelector('.bg-red-600');
-        if(alerta){
+        if(alerta) {
             alerta.remove();
         }
     }
@@ -101,15 +113,22 @@ document.addEventListener('DOMContentLoaded', function() {
     }
 
     function comprobarEmail() {
-
-        // object.values lo que hace es retornar un array con los valores de cada parametro del objeto 
-        // si pusieramos .keys devolveria los parametros, solamente el nombre
-       if (Object.values(email).includes('')){
-        btnSubmit.classList.add('opacity-50');
-        btnSubmit.disabled = true;
-       } else {
+        if(Object.values(email).includes('')) {
+            btnSubmit.classList.add('opacity-50');
+            btnSubmit.disabled = true;
+            return
+        } 
         btnSubmit.classList.remove('opacity-50');
         btnSubmit.disabled = false;
-       };
+    }
+
+    function resetFormulario() {
+        // reiniciar el objeto
+        email.email = '';
+        email.asunto = '';
+        email.mensaje = '';
+
+        formulario.reset();
+        comprobarEmail();
     }
 });
